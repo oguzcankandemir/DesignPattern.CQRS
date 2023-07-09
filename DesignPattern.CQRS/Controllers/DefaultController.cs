24ae1dd5508724a1985bce309fd7ad974fca1674
Oğuzcan Kandemir <90668203+oguzcankandemir@users.noreply.github.com>
2023-07-10 00:24:00 +0300

    [fix] CQRSDesignPattern Crud Operations added Update and Delete Function

--- DesignPattern.CQRS/Controllers/DefaultController.cs
+++ DesignPattern.CQRS/Controllers/DefaultController.cs
@@ -14,12 +14,18 @@ namespace DesignPattern.CQRS.Controllers
         private readonly GetProductQueryHandler _getProductQueryHandler;
         private readonly CreateProductCommandHandler _createProductCommandHandler;
         private readonly GetProductByIDQueryHandler _getProductByIDQueryHandler;
+        private readonly RemoveProductCommandHandler _removeProductCommandHandler;
+        private readonly GetProductUpdateByIDQueryHandler _getProductUpdateByIDQueryHandler;
+        private readonly UpdateProductCommandHandler _updateProductCommandHandler;
 
-        public DefaultController(GetProductQueryHandler getProductQueryHandler, CreateProductCommandHandler createProductCommandHandler, GetProductByIDQueryHandler getProductByIDQueryHandler)
+        public DefaultController(GetProductQueryHandler getProductQueryHandler, CreateProductCommandHandler createProductCommandHandler, GetProductByIDQueryHandler getProductByIDQueryHandler, RemoveProductCommandHandler removeProductCommandHandler, GetProductUpdateByIDQueryHandler getProductUpdateByIDQueryHandler, UpdateProductCommandHandler updateProductCommandHandler)
         {
             _getProductQueryHandler = getProductQueryHandler;
             _createProductCommandHandler = createProductCommandHandler;
             _getProductByIDQueryHandler = getProductByIDQueryHandler;
+            _removeProductCommandHandler = removeProductCommandHandler;
+            _getProductUpdateByIDQueryHandler = getProductUpdateByIDQueryHandler;
+            _updateProductCommandHandler = updateProductCommandHandler;
         }
 
         public IActionResult Index()
@@ -38,11 +44,28 @@ namespace DesignPattern.CQRS.Controllers
             _createProductCommandHandler.Handle(command);
             return RedirectToAction("Index");
         }
-     
+
         public IActionResult GetProduct(int id)
         {
             var values = _getProductByIDQueryHandler.Handle(new GetProductByIDQuery(id));
             return View(values);
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            _removeProductCommandHandler.Handle(new RemoveProductCommand(id));
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var values = _getProductUpdateByIDQueryHandler.Handle(new GetProductUpdateByIDQuery(id));
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(UpdateProductCommand command)
+        {
+            _updateProductCommandHandler.Handle(command);
+            return RedirectToAction("Index");
         }
     }
 }
